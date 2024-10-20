81bc52eee3dfb1cb6e341c8e445c2884146e72db
Finn <112250336+finn200102@users.noreply.github.com>
2024-10-20 13:17:39 +0200

    Add basci html style to books and layout

--- books.js
+++ books.js
@@ -19,6 +19,10 @@ addBookToLibary(book1);
 addBookToLibary(book2);
 addBookToLibary(book3);
 addBookToLibary(book4);
+addBookToLibary(book1);
+addBookToLibary(book2);
+addBookToLibary(book3);
+addBookToLibary(book4);
 
 function displayLibary() {
   const content = document.querySelector(".content");
@@ -26,8 +30,22 @@ function displayLibary() {
     const book = myLibary[i];
     console.log(book);
     const bookContainer = document.createElement("div");
-    bookContainer.textContent = book.author;
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+    const bookInfo = document.createElement("ul");
+    const author = document.createElement("li");
+    author.textContent = `Author: ${book.author}`;
+    const numberOfPages = document.createElement("li");
+    numberOfPages.textContent = `numberOfPage: ${book.numberOfPages}`;
+    const readingStatus = document.createElement("li");
+    readingStatus.textContent = `reading:${book.readingStatus}`;
     bookContainer.classList.add("book-container");
+    bookContainer.appendChild(title);
+    bookInfo.appendChild(author);
+    bookInfo.appendChild(numberOfPages);
+    bookInfo.appendChild(readingStatus);
+    bookInfo.classList.add("book-info");
+    bookContainer.appendChild(bookInfo);
     content.appendChild(bookContainer);
   }
 }
